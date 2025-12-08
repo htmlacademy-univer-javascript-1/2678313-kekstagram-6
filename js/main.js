d13c25523c45ffd617771fecad2ae15f81269cb9
BauerAlexandr <bauer4921@gmail.com>
2025-12-08 21:06:36 +0700

    Исправлен регистр

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { getPosts } from './data.js';
 import { renderPictures } from './miniatures.js';
 
-const Posts = getPosts();
-renderPictures(Posts);
+const posts = getPosts();
+renderPictures(posts);
