bb91886a8791472743bf22f4600cf74bbf3b3333
BauerAlexandr <bauer4921@gmail.com>
2025-12-21 23:50:48 +0700

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { getPosts } from './data.js';
 import { renderPictures } from './miniatures.js';
 import { initBigPicture } from './big-picture.js';
+import { initImageForm } from './form.js';
 
 const posts = getPosts();
 renderPictures(posts);
 initBigPicture(posts);
+initImageForm();
